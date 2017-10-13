60a95dcaefa90d7e5f523064b8372c71285c164e
root <root@t4.test.bjdt.qihoo.net>
2017-10-13 19:18:27 +0800

    init

---
+++ local_settings.py
@@ -0,0 +1,16 @@
+
+CKEDITOR_FILENAME_GENERATOR = 'utils.get_filename'
+CKEDITOR_UPLOAD_PATH = "article_images"
+
+
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source']
+        ]
+    }
+}
---
+++ mcms/utils.py
@@ -0,0 +1,6 @@
+
+
+
+
+def get_filename(filename):
+    return filename.upper()
